118b70d7091ee6f13474cdf7dba863d8c7a22dfb
Huisman <kwl19930210@sina.com>
2017-10-14 23:30:47 +0800

    nodejs code

---
+++ demo-event.js
@@ -0,0 +1,20 @@
+﻿var events = require("events");
+
+var eventEmiter = new events.EventEmitter();
+
+
+var connectHandler = function connected (){
+	console.log("connect sucess!");
+	
+	eventEmiter.emit("data_received");
+}
+
+
+eventEmiter.on('connection',connectHandler );
+
+eventEmiter.on('data_received',function(){
+console.log('data recevied suceess!')});
+
+eventEmiter.emit('connection');
+
+console.log('finished!')
